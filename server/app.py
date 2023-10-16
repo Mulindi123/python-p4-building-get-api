5a786eddfc76b3ea3f299e2da5ee501895c0ed58
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-10-16 11:40:02 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# app/app.py
 
 from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
@@ -18,6 +18,93 @@ db.init_app(app)
 @app.route('/')
 def index():
     return "Index for Game/Review/User API"
+
+@app.route('/games')
+def games():
+
+    games = []
+    for game in Game.query.all():
+        game_dict = {
+            "title": game.title,
+            "genre": game.genre,
+            "platform": game.platform,
+            "price": game.price,
+        }
+        games.append(game_dict)
+
+    response = make_response(
+        jsonify(games),
+        200
+    )
+
+    return response
+
+@app.route("/games/<int:id>")
+def game_by_id(id):
+    game = Game.query.filter_by(id=id).first()
+    
+
+    # game_dict ={
+    #     "title": game.title,
+    #     "genre": game.genre,
+    #     "platform": game.platform,
+    #     "price": game.price
+    # }
+
+    game_dict = game.to_dict()
+    
+    response = make_response(jsonify(game_dict), 200)
+
+    response.headers["Content_Type"] = "application/json"
+    return response
+
+#Show games sorted by title
+# @app.route('/games')
+# def games():
+#     games = Game.query.order_by(Game.title).all()  # Sort games by title
+#     games_list = [
+#         {
+#             "title": game.title,
+#             "genre": game.genre,
+#             "platform": game.platform,
+#             "price": game.price,
+#         }
+#         for game in games
+#     ]
+
+#     response = make_response(
+#         jsonify(games_list),
+#         200
+#     )
+
+#     return response
+
+#Only show the first 10 games
+# @app.route('/games')
+# def games():
+#     games = Game.query.limit(10).all()  # Sort games by title and limit to 10
+#     games_list = [
+#         {
+#             "title": game.title,
+#             "genre": game.genre,
+#             "platform": game.platform,
+#             "price": game.price,
+#         }
+#         for game in games
+#     ]
+
+#     response = make_response(
+#         jsonify(games_list),
+#         200
+#     )
+
+#     return response
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
